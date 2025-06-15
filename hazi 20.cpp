409d7624ea06f61a6284bbdca2af4c82d913329d
akodolo <148947463+KocsisPatakiSimon@users.noreply.github.com>
2025-06-15 22:17:48 +0300

    Update hazi 20.cpp

--- hazi 20.cpp
+++ hazi 20.cpp
@@ -1,47 +1,28 @@
+
 #include <iostream>
-#include <string>
+#include <fstream>
 
-struct ListaElem {
-    int szam;
-    std::string szoveg;
-    ListaElem* kov;
-};
+using namespace std;
 
-int main() {
-    int n;
-    std::cin >> n;
-
-    ListaElem* fej = nullptr;
-    ListaElem* utolso = nullptr;
-
-    for (int i = 0; i < n; ++i) {
-        ListaElem* uj = new ListaElem;
-        std::cin >> uj->szam >> uj->szoveg;
-        uj->kov = nullptr;
-
-        if (fej == nullptr) {
-            fej = uj;
-        } else {
-            utolso->kov = uj;
-        }
-        utolso = uj;
+int main()
+{
+    ofstream out ("titu2022.out");
+    int x, y, fn = 1, n = 0;
+    cout << "x= ";
+    cin >> x;
+    cout << "y= ";
+    cin >> y;
+    while (fn < y)
+    {
+        n++;
+        fn = fn + 2 * n;
     }
-
-    ListaElem* maxElem = fej;
-    for (ListaElem* ptr = fej->kov; ptr != nullptr; ptr = ptr->kov) {
-        if (ptr->szam > maxElem->szam) {
-            maxElem = ptr;
-        }
+    while (fn >= x)
+    {
+        out << fn << " ";
+        fn = fn - 2 * n;
+        n--;
     }
-
-    std::cout << maxElem->szoveg << "\n";
-
-    // mem�ria felszabad�t�sa
-    while (fej != nullptr) {
-        ListaElem* temp = fej;
-        fej = fej->kov;
-        delete temp;
-    }
-
+    out.close();
     return 0;
 }
